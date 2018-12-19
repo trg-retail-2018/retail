38b966bd6e57dc1bac5a3d1b18a5c72009d7d11f
shinga <arthur.shing@gmail.com>
2018-12-19 15:23:23 -0500

    Working initial load into avro

--- initial_load.py
+++ initial_load.py
@@ -4,8 +4,7 @@ from pyspark.sql import SparkSession, SQLContext
 
 
 # Run script by using:
-# spark-submit --packages mysql:mysql-connector-java:5.1.39 [scriptname]
-
+# spark-submit --packages mysql:mysql-connector-java:5.1.39,com.databricks:spark-avro_2.11:4.0.0 initial_load.py
 
 #Main function
 def main():
@@ -24,9 +23,33 @@ def main():
 	    dbtable = "as_promotion",
 	    user="retail_dba",
 	    password="itversity").load()
-	
+
+
+	sales_1997_df = sqlContext.read.format("jdbc").options(
+            url="jdbc:mysql://nn01.itversity.com:3306/retail_export",
+            driver = "com.mysql.jdbc.Driver",
+            dbtable = "as_sales_1997",
+            user="retail_dba",
+            password="itversity").load()
+
+
+
+	sales_1998_df = sqlContext.read.format("jdbc").options(
+            url="jdbc:mysql://nn01.itversity.com:3306/retail_export",
+            driver = "com.mysql.jdbc.Driver",
+            dbtable = "as_sales_1998",
+            user="retail_dba",
+            password="itversity").load()	
 	# Just a print statement to see if the dataframe transferred sucessfully
 	print promotion_df.show()
+	print sales_1997_df.show()
+	print sales_1998_df.show()
+
+	promotion_df.write.format("com.databricks.spark.avro").save("file:///home/arthurshing/foodmart/case_study/raw/promotion")
+	sales_1997_df.write.format("com.databricks.spark.avro").save("file:///home/arthurshing/foodmart/case_study/raw/sales97")
+	sales_1998_df.write.format("com.databricks.spark.avro").save("file:///home/arthurshing/foodmart/case_study/raw/sales98")
+
+
 
 # Runs the script
 if __name__ == "__main__":
