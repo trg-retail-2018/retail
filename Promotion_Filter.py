4bf623f524f59e97777d806395ee9bcc76df6ee5
Arthur Shing <arthur.shing@gmail.com>
2018-12-21 14:58:40 -0800

    Refactored column name

--- Promotion_Filter.py
+++ Promotion_Filter.py
@@ -13,31 +13,33 @@ import os
 def main():
 
 
-    spark = SparkSession.builder.master("local").appName("Initial Load").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Initial Load").getOrCreate()
+last_update_column_name = 'last_update_date'
 
-    hostname = "nn01.itversity.com"
-    port = "3306"
-    connection = "jdbc:mysql://"
-    dbname = "retail_export"
-    readdriver = "com.mysql.jdbc.Driver"
-    username = "retail_dba"
-    password = "itversity"
 
-    # Read the last updated dates from a file
-    home = os.path.expandvars("$HOME")
+hostname = "nn01.itversity.com"
+port = "3306"
+connection = "jdbc:mysql://"
+dbname = "retail_export"
+readdriver = "com.mysql.jdbc.Driver"
+username = "retail_dba"
+password = "itversity"
 
-    # STEP 2 - Load S3 Raw-Bucket to Cleansing S3 Raw-Bucket
-    # *******************************************************************
-    # *******************************************************************
-    # *******************************************************************
-    promotion_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/promotion/")
-    pro_df=promotion_RF_df.where(col("promotion_id") > 0).drop('last_update_column')
+# Read the last updated dates from a file
+home = os.path.expandvars("$HOME")
+
+# STEP 2 - Load S3 Raw-Bucket to Cleansing S3 Raw-Bucket
+# *******************************************************************
+# *******************************************************************
+# *******************************************************************
+promotion_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/promotion/")
+pro_df=promotion_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
     sales_1997_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/sales97/")
-    sales97_df=sales_1997_RF_df.where(col("promotion_id") > 0).drop('last_update_column')
+    sales97_df=sales_1997_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
     sales_1998_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/sales98/")
-    sales98_df=sales_1998_RF_df.where(col("promotion_id") > 0).drop('last_update_column')
+    sales98_df=sales_1998_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
     # sales_1998_dec_RF_df=spark.read.format("com.databricks.spark.avro").load(
     #     "file://" + home + "/foodmart/case_study/raw/sales98dec/")
