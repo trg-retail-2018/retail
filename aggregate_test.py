486271feb3bf6ca7e4c987df5bbcf4238f9ffc74
Arthur Shing <arthur.shing@gmail.com>
2018-12-21 14:30:04 -0800

    refactored aggregate

--- aggregate_test.py
+++ aggregate_test.py
@@ -13,26 +13,33 @@ import datetime
 
 def main():
 
-	# Set up spark context 
+	# Set up spark context
 
 	spark = SparkSession.builder.master("local").appName("Initial Load").getOrCreate()
 
+	hostname = "nn01.itversity.com"
+	port = "3306"
+	dbname = "retail_export"
+	readdriver = "com.mysql.jdbc.Driver"
+	username = "retail_dba"
+	password = "itversity"
+
 	parquet_df = spark.read.format("parquet").load("/home/cloudera/Shwetha/case_study/parquet/")
 	print parquet_df.show()
-	
+
 	timeByDay_df = spark.read.format("jdbc").options(
-	    url="jdbc:mysql://localhost:3306/foodmart",
-	    driver = "com.mysql.jdbc.Driver",
-	    dbtable = "time_by_day",
-	    user="root",
-	    password="cloudera").load()
+	    url="jdbc:mysql://" + hostname + ":" + port + "/" + dbname,
+	    driver = readdriver,
+	    dbtable = "as_time_by_day",
+	    user=username,
+	    password=password).load()
 
 	store_df = spark.read.format("jdbc").options(
-	    url="jdbc:mysql://localhost:3306/foodmart",
-	    driver = "com.mysql.jdbc.Driver",
-	    dbtable = "store",
-	    user="root",
-	    password="cloudera").load()
+	    url="jdbc:mysql://" + hostname + ":" + port + "/" + dbname,
+	    driver = readdriver,
+	    dbtable = "as_store",
+	    user=username,
+	    password=password).load()
 
 	print timeByDay_df.show()
 	print store_df.show()
@@ -43,7 +50,7 @@ def main():
 	print parquet_time_df.show()
 	print prq_time_store_df.show()
 
-	
+
 
 
 
@@ -52,12 +59,12 @@ def main():
 	sales97_df = sales97_RF_df.where(col("promotion_id") > 0).drop("lst_upd_date")
 	sales98_RF_df = spark.read.format("com.databricks.spark.avro").load("/home/cloudera/Shwetha/case_study/raw/sales98/")
 	sales98_df = sales98_RF_df.where(col("promotion_id") > 0).drop("lst_upd_date")
-	
+
 
 	print pro_df.show()
 	print sales97_df.show()
-	print sales98_df.show()	
-	
+	print sales98_df.show()
+
 	sales_DF = sales97_df.unionAll(sales98_df)
 	final_DF = pro_df.join(sales_DF, pro_df.promotion_id == sales_DF.promotion_id).drop(sales_DF.promotion_id)
 	#final_DF.write.mode(SaveMode.Append).format("parquet").save("/home/cloudera/Shwetha/case_study/parquet")
@@ -66,7 +73,7 @@ def main():
 	print sales_DF.show()
 	print final_DF.show()"""
 
-	
+
 # Runs the script
 if __name__ == "__main__":
 	main()
