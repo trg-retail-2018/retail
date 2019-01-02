960977f8bcfea6dc5182a750ab80d5c9d5776014
Arthur Shing <arthur.shing@gmail.com>
2019-01-02 10:36:54 -0800

    refactored promotion filter

--- as_promotion_filter.py
+++ as_promotion_filter.py
@@ -17,28 +17,27 @@ def main():
     last_update_column_name = 'last_update_date'
 
 
-    hostname = "nn01.itversity.com"
+    hostname = "localhost"
     port = "3306"
     connection = "jdbc:mysql://"
-    dbname = "retail_export"
+    dbname = "foodmart"
     readdriver = "com.mysql.jdbc.Driver"
-    username = "retail_dba"
-    password = "itversity"
+    username = "root"
+    password = "mysql"
 
-    # Read the last updated dates from a file
-    home = os.path.expandvars("$HOME")
+    destination_path = "file:///mnt/c/Users/Arthur/Documents/retail_ensoftek/buckets/"
 
     # STEP 2 - Load S3 Raw-Bucket to Cleansing S3 Raw-Bucket
     # *******************************************************************
     # *******************************************************************
     # *******************************************************************
-    promotion_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/promotion/")
+    promotion_RF_df=spark.read.format("com.databricks.spark.avro").load(destination_path + "raw/promotion/")
     pro_df=promotion_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
-    sales_1997_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/sales97/")
+    sales_1997_RF_df=spark.read.format("com.databricks.spark.avro").load(destination_path + "raw/sales97/")
     sales97_df=sales_1997_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
-    sales_1998_RF_df=spark.read.format("com.databricks.spark.avro").load("file://" + home + "/foodmart/case_study/raw/sales98/")
+    sales_1998_RF_df=spark.read.format("com.databricks.spark.avro").load(destination_path + "raw/sales98/")
     sales98_df=sales_1998_RF_df.where(col("promotion_id") > 0).drop(last_update_column_name)
 
     # sales_1998_dec_RF_df=spark.read.format("com.databricks.spark.avro").load(
@@ -50,7 +49,7 @@ def main():
     final_DF=pro_df.join(sales_DF, pro_df.promotion_id == sales_DF.promotion_id).drop(sales_DF.promotion_id)
 
     # MAKE SURE YOU CREATE THE DIRECTORY FIRST, BECAUSE THIS IS APPENDING
-    final_DF.write.mode("append").format("parquet").save("file://" + home + "/foodmart/case_study/cleansed/")
+    final_DF.write.mode("append").format("parquet").save(destination_path + "cleansed/")
 
     # *******************************************************************
     # *******************************************************************
